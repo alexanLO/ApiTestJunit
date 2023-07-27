39ff0fcffd6c3020ca54a94f6c13b863aa2b495a
Alexan Lima <alexan.oliveira@capgemini.com>
2023-07-27 11:36:11 -0300

    classe user resource findById

--- src/main/java/br/com/api/apitestjunit5/resource/UsersResource.java
+++ src/main/java/br/com/api/apitestjunit5/resource/UsersResource.java
@@ -1,5 +1,6 @@
 package br.com.api.apitestjunit5.resource;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -7,13 +8,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.api.apitestjunit5.domain.Users;
+import br.com.api.apitestjunit5.service.UsersService;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UsersResource {
     
+    @Autowired
+    private UsersService usersService;
+
     @GetMapping(value = "/{id}")
     public ResponseEntity<Users> findByID(@PathVariable Integer id){
-        return ResponseEntity.ok().body(new Users(1, "Valdir", "valdir@email.com", "123"));
+        return ResponseEntity.ok().body(usersService.findByID(id));
     }
 }
