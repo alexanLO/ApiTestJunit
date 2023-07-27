d63e4329cba54c8e7753ab68e9a7e43bca382001
Alexan Lima <alexan.oliveira@capgemini.com>
2023-07-27 11:34:08 -0300

    classe user service impl findById

---
+++ src/main/java/br/com/api/apitestjunit5/service/impl/UsersServiceImp.java
@@ -0,0 +1,24 @@
+package br.com.api.apitestjunit5.service.impl;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.api.apitestjunit5.domain.Users;
+import br.com.api.apitestjunit5.repository.UsersRepository;
+import br.com.api.apitestjunit5.service.UsersService;
+
+@Service
+public class UsersServiceImp implements UsersService{
+
+    @Autowired
+    private UsersRepository usersRepository;
+
+    @Override
+    public Users findByID(Integer id) {
+       Optional<Users> obj = usersRepository.findById(id);
+       return obj.orElse(null);
+    }
+    
+}
